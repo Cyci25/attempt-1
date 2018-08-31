25a311f540533cc5ae082863e608f7209cf0e123
Cyci25 <c2muriithi@gmail.com>
2018-08-31 16:41:13 +0300

    First attempt

--- app/config.py
+++ app/config.py
@@ -1,12 +1,9 @@
 class Config:
-    pass
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cda6b2edbf2c491d8582d355c7442a3f'
 
 class ProdConfig(Config):
     """docstring for ProdConfig."""
     pass
-    # def __init__(self, arg):
-    #     super(ProdConfig, self).__init__()
-    #     self.arg = arg
 
 class DevConfig(Config):
     DEBUG = True
--- app/views.py
+++ app/views.py
@@ -1,15 +1,25 @@
+# Views
 from flask import render_template
 from app import app
 
 @app.route('/')
 def index():
-    message = 'Hello World!'
-    return render_template('index.html', message1 = message)
-    title = 'Home - News Highlights'
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    message = 'Hello World'
+    return render_template('index.html',message = message)
+
+    title = 'Home - News!!'
     return render_template('index.html', title = title)
 
-@app.route('/news/<news_id>')
+@app.route('/news/<int:news_id>')
 def news(news_id):
-    return render_template('news.html', id = news_id )
-    heading = 'News - More News!'
-    return render_template('news.html', heading = heading )
+
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    heading = 'Hell no?'
+    return render_template('news.html',id = heading)
